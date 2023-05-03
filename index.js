cfb37dfbdd2a3a6f117b6f4f2d4962b8bb6ba4ec
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-05-03 08:44:22 +0600

    create API for chefinfo

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chefinfo = require('./data/chefinfo.json');
+const recipes = require('./data/recipes.json');
+
 app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('Cooking Studio is Running');
 });
+
+
+app.get('/chefinfo', (req, res) => {
+    res.send(chefinfo);
+});
+
+
 
 
 app.listen(port, () =>{
